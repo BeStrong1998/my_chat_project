9f7d8fafdd3fce736fd30377325f6ac6f736a7ab
Oleg <oamalaxov@mail.ru>
2024-12-03 13:23:45 +0300

    Добавил библиотеку SocketIO и создал объект для SocketIO

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from flask_socketio import SocketIO
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,3 +10,13 @@ app = Flask(__name__)
 # Настройка базы данных SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 db = SQLAlchemy(app)
+
+socketio = SocketIO(app)
+
+
+# # Обработчик сообщений от клиентов
+# @socketio.on('message')
+# def handle_message(msg):
+#     print('Message:', msg)
+#     # Отправляем сообщение всем подключённым клиентам
+#     send(msg, broadcast=True)
