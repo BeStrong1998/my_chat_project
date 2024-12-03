1b8bd4763f4c14a72f872b85ed2c196a1d4b0e9b
Oleg <oamalaxov@mail.ru>
2024-12-03 13:35:30 +0300

    Добавил объект socketio для запуска сервера с поддержкой вебсокетов с информацией об ошибках если имеются

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from app import socketio
+
 from controller import app
 
 
 if __name__ == '__main__':
-    app.run()
+    # Запуск сервера с поддержкой вебсокетов с информацией об ошибках если имеются
+    socketio.run(app, debug=True)
+    # app.run()
