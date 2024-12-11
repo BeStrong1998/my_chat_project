c4948e66e249e552571d52a5529ceb6953badb75
Oleg <oamalaxov@mail.ru>
2024-12-11 14:37:52 +0300

    Добавление комментариев

--- business_logic/check_fata.py
+++ business_logic/check_fata.py
@@ -1,4 +1,6 @@
+ # Функция для проверки валидности логина и пароля
 def check_auth_data(login: str, password: str):
+    # Проверяем длину логина и пароля
     if not (3 < len(login) < 20) or not (4 < len(password) < 32):
         return False
     return True
