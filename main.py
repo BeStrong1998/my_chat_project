0c4a7f417631ebff04bddb3c6eeff01b180d494a
Oleg <oamalaxov@mail.ru>
2024-12-16 08:19:38 +0300

    Добавлен from errors import app и убрал debag=True

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from app import socketio
 
-from controller import app
+# from controller import app
+from errors import app
 
 
 if __name__ == '__main__':
     # Запуск сервера с поддержкой вебсокетов с информацией об ошибках если имеются
-    socketio.run(app, debug=True)
+    socketio.run(app)   #, debug=True)
